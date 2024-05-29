771a22785e2a5ba5d5abb96fb08c95fc7520f243
MinhTris1705 <trinm1705@gmail.com>
2024-05-29 11:30:29 +0700

    gui

---
+++ d3_visulaizations.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Median Age Graph
+    d3.json('https://api.github.com/...', function(data) {
+        // Parse data and create graph
+        // D3 graph code here
+    });
+
+    // Fertility Rate Graph
+    d3.json('https://api.github.com/...', function(data) {
+        // Parse data and create graph
+        // D3 graph code here
+    });
+
+    // Age Structure Graph
+    d3.json('https://api.github.com/...', function(data) {
+        // Parse data and create graph
+        // D3 graph code here
+    });
+});
